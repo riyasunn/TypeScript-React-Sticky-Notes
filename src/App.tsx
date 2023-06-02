c5649b32b5202984dcd74e272779ed95a6894327
Riya Sun <riya.sunn@gmail.com>
2023-06-02 16:02:27 +1200

    Initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Container>
+      <Typography textAlign='center' variant='h3' mt={3} mb={5}>
+        Sticky Notes
+      </Typography>
+      <Button>Add note...</Button>
+      <Button>Generate chart</Button>
+
+      <Grid>
+
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+import { AnyAction } from '@reduxjs/toolkit';
